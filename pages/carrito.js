5acda03f53de1f38e377b0a90e1fa7748d0257fc
eritalla <erica.tallapietra@gmail.com>
2023-04-07 02:42:53 -0300

    se parsea desde localstorage

---
+++ pages/carrito.js
@@ -0,0 +1,6 @@
+
+const prodsEnCarrito = JSON.parse(localStorage.getItem("prod-seleccionado"));
+console.log(prodsEnCarrito);
+
+const vacio = document.querySelector("#vacio");
+const productosCarrito = document.querySelector("#productosCarrito");
