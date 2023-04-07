00e61bc983622fc772c04928376a307b8eb34170
eritalla <erica.tallapietra@gmail.com>
2023-04-07 11:46:48 -0300

    se cargan productos al carrito

--- pages/carrito.js
+++ pages/carrito.js
@@ -3,4 +3,24 @@ const prodsEnCarrito = JSON.parse(localStorage.getItem("prod-seleccionado"));
 console.log(prodsEnCarrito);
 
 const vacio = document.querySelector("#vacio");
-const productosCarrito = document.querySelector("#productosCarrito");
+const productosCarrito = document.querySelector("#productosAgregadosCarrito");
+const carritoBotones = document.querySelector("#carritoBotones");
+const agradecimiento = document.querySelector("#agradecimiento");
+
+
+if (prodsEnCarrito) {
+    vacio.innerHTML = "";
+
+    prodsEnCarrito.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("prodCarrito");
+        div.innerHTML = `
+        <p>Producto: ${producto.nombre}</p>
+        <p>Precio:${producto.precio}</p>
+        <p>Cantidad:${producto.cantidad}</p>
+        <p>Subtotal:${producto.precio * producto.cantidad}</p>
+        <button id="${producto.id}">Eliminar del carrito</button>
+        `
+        productosCarrito.append(div);
+    });
+}
